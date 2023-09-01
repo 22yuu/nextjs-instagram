cadab6526688f473832034d9c6fd697c63a07c00
22yuu <22yuu@naver.com>
2023-09-01 22:21:01 +0900

    feat: Next-Auth 구글 로그인 기능 구현 및 로그인 페이지 구현

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { getServerSession } from 'next-auth';
+import { getProviders } from 'next-auth/react';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+import Signin from '@/components/Signin';
+
+type Props = {
+  searchParams: {
+    callbackUrl: string;
+  };
+};
+
+export default async function SignInPage({
+  searchParams: { callbackUrl },
+}: Props) {
+  const session = await getServerSession(authOptions);
+  console.log(callbackUrl);
+
+  if (session) {
+    redirect('/');
+  }
+
+  const providers = (await getProviders()) ?? {};
+
+  return (
+    <section className="flex justify-center mt-24">
+      <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
+    </section>
+  );
+}
